8c327f7cf145f793cea8a3d1482e6497cfb74f11
Suehyun Baig <suehyun210@gmail.com>
2026-01-13 18:41:04 -0800

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -1,4 +1,11 @@
+-- solution 1: using modulo operator %
 SELECT *
 FROM cinema
 WHERE id%2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
+
+-- solution 2: using mod() function
+SELECT *
+FROM cinema
+WHERE mod(id,2) = 1 AND description != 'boring'
 ORDER BY rating DESC;
