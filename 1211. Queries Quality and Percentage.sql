763d591cf65da950d07da651e0a99ff7bfe01775
Suehyun Baig <suehyun210@gmail.com>
2026-04-07 20:40:15 -0700

    Create 1211. Queries Quality and Percentage.sql

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select query_name, 
+round(avg(rating::numeric/position), 2) as quality,
+round((count(*) filter (where rating < 3)::numeric/count(*))*100, 2) as poor_query_percentage
+from queries
+group by query_name
