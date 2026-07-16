baca83b544df4062b8019cd95ff98a11778e536c
Suehyun Baig <suehyun210@gmail.com>
2026-07-15 21:53:53 -0700

    Clarify edge case handling in second highest salary query
    Added comments to clarify handling of edge cases for second highest salary query.

--- Subqueries & CTE/176. Second Highest Salary.sql
+++ Subqueries & CTE/176. Second Highest Salary.sql
@@ -4,4 +4,6 @@ SELECT
      ORDER BY salary DESC
      LIMIT 1 OFFSET 1) AS SecondHighestSalary
 
-
+-- The question asks to return one row with value 'null' when there is no second highest salary
+-- Filtering with where statement will result in 0 row in edge cases (e.g. all employees have the same salary)
+-- Therefore, we should approach this with a scalar subquery, which always returns one value (including null)
