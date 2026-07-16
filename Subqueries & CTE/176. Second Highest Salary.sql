d487b9b1ae149f7879e70a19512b4dd9148d5d7a
Suehyun Baig <suehyun210@gmail.com>
2026-07-15 21:55:29 -0700

    Enhance comments for second highest salary query
    Added comments to clarify the approach for retrieving the second highest salary, including handling edge cases.

--- Subqueries & CTE/176. Second Highest Salary.sql
+++ Subqueries & CTE/176. Second Highest Salary.sql
@@ -7,3 +7,6 @@ SELECT
 -- The question asks to return one row with value 'null' when there is no second highest salary
 -- Filtering with where statement will result in 0 row in edge cases (e.g. all employees have the same salary)
 -- Therefore, we should approach this with a scalar subquery, which always returns one value (including null)
+
+-- "limit 1 offset 1" means to exclude 1 and get the next 1
+-- "distinct" makes sure there is no duplicate before excluding 1 (the highest)
