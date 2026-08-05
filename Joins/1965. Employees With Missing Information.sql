001940614803425e73a882edf93c47628da9bed9
Suehyun Baig <suehyun210@gmail.com>
2026-08-05 00:03:17 -0700

    Enhance SQL query with comments on joins
    Added comments to clarify join types and COALESCE usage.

--- Joins/1965. Employees With Missing Information.sql
+++ Joins/1965. Employees With Missing Information.sql
@@ -2,3 +2,6 @@ select coalesce(e.employee_id, s.employee_id) as employee_id
 from employees as e full join salaries as s on e.employee_id = s.employee_id
 where (e.name is null) or (s.salary is null)
 order by coalesce(e.employee_id, s.employee_id)
+
+-- left join | right join | full join | join (= inner join)
+-- when doing a full join, make sure to use COALESCE to refer to the key column
