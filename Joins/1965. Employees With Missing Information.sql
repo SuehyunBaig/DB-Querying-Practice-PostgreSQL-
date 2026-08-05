04babc97d75c8906702cbcac3dcfdadfd4aee5d6
Suehyun Baig <suehyun210@gmail.com>
2026-08-05 00:00:04 -0700

    Add SQL query for employees with missing info

---
+++ Joins/1965. Employees With Missing Information.sql
@@ -0,0 +1,4 @@
+select coalesce(e.employee_id, s.employee_id) as employee_id
+from employees as e full join salaries as s on e.employee_id = s.employee_id
+where (e.name is null) or (s.salary is null)
+order by coalesce(e.employee_id, s.employee_id)
