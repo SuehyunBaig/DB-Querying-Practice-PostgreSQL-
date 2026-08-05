6c9577ce418c0339428e034c3735da1996ec46f8
Suehyun Baig <suehyun210@gmail.com>
2026-08-04 20:50:09 -0700

    Update 176. Second Highest Salary.sql

--- Subqueries & CTE/176. Second Highest Salary.sql
+++ Subqueries & CTE/176. Second Highest Salary.sql
@@ -2,7 +2,7 @@ SELECT
     (SELECT DISTINCT salary
      FROM employee
      ORDER BY salary DESC
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary
+     OFFSET 1 LIMIT 1) AS SecondHighestSalary
 
 -- The question asks to return one row with value 'null' when there is no second highest salary
 -- Filtering with where statement will result in 0 row in edge cases (e.g. all employees have the same salary)
