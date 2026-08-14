c0c770e3325fa89372b462d7c194f18ca2c8b7b7
Suehyun Baig <suehyun210@gmail.com>
2026-08-13 21:53:28 -0700

    Correct rank calculation in restaurant growth SQL query
    Fix rank calculation in moving sum query and adjust formatting.

--- Window Functions/1321. Restaurant Growth.sql
+++ Window Functions/1321. Restaurant Growth.sql
@@ -1,12 +1,12 @@
 select visited_on, moving_sum as amount, round((moving_sum/7), 2) as average_amount
 from (select visited_on, 
       sum(daily_amount) over (order by visited_on rows between 6 preceding and current row) as moving_sum,
-      rank() over (order by visited_on)
+      rank() over (order by visited_on) as rank
       from(select visited_on, sum(amount) as daily_amount
            from customer
-           group by visited_on)
-      order by visited_on asc) 
+           group by visited_on)) 
 where rank >= 7
+order by visited_on asc
 
 -- First, aggregate daily sales
 -- Leveraging daily sales column get 7-day moving total: sum(daily_amount) over (order by visited_on rows between 6 preceding and current row) as moving_sum
