71c20fd4b434b6e9c9e72e3870d8ea01c18ffeb1
Suehyun Baig <suehyun210@gmail.com>
2026-08-13 21:48:47 -0700

    Create 1321. Restaurant Growth.sql

---
+++ Window Functions/1321. Restaurant Growth.sql
@@ -0,0 +1,14 @@
+select visited_on, moving_sum as amount, round((moving_sum/7), 2) as average_amount
+from (select visited_on, 
+      sum(daily_amount) over (order by visited_on rows between 6 preceding and current row) as moving_sum,
+      rank() over (order by visited_on)
+      from(select visited_on, sum(amount) as daily_amount
+           from customer
+           group by visited_on)
+      order by visited_on asc) 
+where rank >= 7
+
+-- First, aggregate daily sales
+-- Leveraging daily sales column get 7-day moving total: sum(daily_amount) over (order by visited_on rows between 6 preceding and current row) as moving_sum
+-- Moving average can easily calculated and rounded at the very last step, so leave it until then
+-- In order to filter the data from 7th day onwards, we need to create rank column too!: rank() over (order by visited_on)
